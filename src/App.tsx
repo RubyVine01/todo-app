015e62269df9391156d75ebe92b120bdbfbc14df
RubyVine01 <leraprokofevaiera1@gmail.com>
2024-11-04 20:42:45 +0200

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
-import { useState } from 'react'
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import AuthPage from "./pages/AuthPage";
+import TodoPage from "./pages/TodoPage";
 
-import './App.css'
-
-function App() {
+const App: React.FC = () => {
+  const isLoggedIn: boolean = localStorage.getItem("loggedIn") === "true";
 
   return (
-    <>
-  
-    </>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/login" element={<AuthPage />} />
+        <Route
+          path="/"
+          element={isLoggedIn ? <TodoPage /> : <Navigate to="/login" />}
+        />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
